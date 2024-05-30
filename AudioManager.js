9950417de090bb12a1202a04499d5d06498687c0
dominic <dominicwhatling@outlook.com>
2024-05-30 23:45:32 +1000

    audio controls & post game effects

--- AudioManager.js
+++ AudioManager.js
@@ -2,23 +2,32 @@
 
 class AudioManager {
 	#obj;
-	/**@private @param {Object<string, HTMLAudioElement>} obj*/
-	constructor(obj) {
+	#mute;
+	/**@private @param {Object<string, HTMLAudioElement>} obj @param {boolean} mute*/
+	constructor(obj, mute) {
 		this.#obj = obj;
+		this.#mute = mute;
 	}
-	/**@param {...{alias: string, filename: string, volume: number}} audioStructs*/
-	static GetAudioManager(...audioStructs) {
+	/**@param {{alias: string, location: string, volume: number}[]} audioStructs @param {boolean} mute*/
+	static GetAudioManager(audioStructs, mute = false) {
 		const obj = {};
-		for (const { alias, filename, volume } of audioStructs) {
+		for (const { alias, location: filename, volume } of audioStructs) {
 			const audio = new Audio(filename);
 			audio.volume = volume;
 			obj[alias] = audio;
 		}
 
-		return new AudioManager(obj);
+		return new AudioManager(obj, mute);
 	}
 	/**@param {string} alias*/
 	Play(alias) {
+		if (this.#mute) return;
 		this.#obj[alias].play();
+	}
+	/**@param {boolean?} override*/
+	ToggleMute(override) {
+		if (typeof override == 'boolean') this.#mute = override;
+
+		this.#mute = !this.#mute;
 	}
 }
--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 /**@type {Plane}*/ let plane;
 let scoreBoard = ScoreBoardManager.GetScoreBoard(document.querySelector('.score-board-container'));
-let audioController = AudioManager.GetAudioManager(
-	{
-		alias: 'point',
-		filename: './sfx/point.wav',
-		volume: 1
-	},
-	{
-		alias: 'jump',
-		filename: './sfx/jump.wav',
-		volume: 1
-	}
-);
+let audioController = AudioManager.GetAudioManager([
+	{ alias: 'point', location: './sfx/point.wav', volume: 1 },
+	{ alias: 'jump', location: './sfx/jump.wav', volume: 1 },
+	{ alias: 'explosion', location: './sfx/explosion.mp3', volume: 1 },
+	{ alias: 'prayer', location: './sfx/allahu_akbar.mp3', volume: 0.1 }
+], false);
 let gameState = false;
 
 let TIME;
 requestAnimationFrame(function loop(time) {//update loop
+	if (!gameState && document.querySelector('.explosion') != null && document.querySelector('.building') != null) {//band-aid fix for building animation deletion
+		if (document.querySelector('.building').getBoundingClientRect().right < document.querySelector('.game-area').getBoundingClientRect().left) {
+			document.querySelector('.game-area').removeChild(document.querySelector('.building'));
+		}
+	}
+
 	if (gameState && time != null) {
 		document.querySelector('.fps-counter').innerHTML = `${Math.round(1000 / (time - TIME))}fps`;
 		TIME = time;
@@ -33,6 +33,12 @@ requestAnimationFrame(function loop(time) {//update loop
 
 window.onload = () => {//start game
 	plane = Plane.CreatePlane({ x: 200, y: 200 }, 11);
+
+	const audioBool = Boolean(Number(localStorage.getItem('audio-settings') ?? 1)); //default on
+	audioController.ToggleMute(audioBool);
+	document.querySelector('.sfx-toggle').dataset['active'] = String(Number(audioBool));
+	document.querySelector('.sfx-toggle').src = (audioBool) ? './img/audio.png' : './img/no-audio.png';
+
 	gameState = true;
 }
 
@@ -42,13 +48,35 @@ function HandlePostGame() {
 	document.body.appendChild(document.querySelector('#game-over-menu-template').content.cloneNode(true));
 	const element = document.querySelector('.game-over-menu');
 
+	const explosion = document.createElement('img');
+	explosion.src = './img/explosion.png';
+	explosion.draggable = false;
+	explosion.className = 'explosion';
+	explosion.style.left = `${plane.Position.x}px`;
+	explosion.style.top = `${plane.Position.y}px`;
+	document.querySelector('.game-area').appendChild(explosion);
+
+	audioController.Play('explosion');
+	audioController.Play('prayer');
+
 	element.querySelector('img').addEventListener('click', () => {
 		location.reload();
 	});
 
 	document.addEventListener('keyup', (e) => {
-		if (e.key == 'Enter') {
+		if (e.key == 'Enter' || e.key == ' ') {
 			location.reload();
 		}
 	});
 }
+
+document.querySelector('.sfx-toggle').addEventListener('click', () => {
+	const newBool = !Boolean(Number(document.querySelector('.sfx-toggle').dataset['active']));
+	document.querySelector('.sfx-toggle').dataset['active'] = String(Number(newBool));
+
+	document.querySelector('.sfx-toggle').src = (newBool) ? './img/audio.png' : './img/no-audio.png';
+
+	localStorage.setItem('audio-settings', Number(newBool));
+
+	audioController.ToggleMute(newBool);
+});
